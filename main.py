9c838e0bd4d72b42f14ce28d55c1681896f4156e
Mario Santos <marioguilherme@tecnico.ulisboa.pt>
2023-12-26 16:15:42 +0000

    main updated

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
         sys.exit(1)
 
     pythonFile = sys.argv[1]
-    vulnerabilities = sys.argv[2]
+    vulFile = sys.argv[2]
     outputFile = "./output/" + pythonFile[:-3] + ".output.json"
 
 
@@ -22,25 +22,41 @@ def main():
     if pythonFile[-3:] != ".py":
         print("First argument should be a python file")
         sys.exit(1)
-    if vulnerabilities[-5:] != ".json":
+    if vulFile[-13:] != "patterns.json":
         print("Second argument should be a JSON file")
         sys.exit(1)
 
-    # try to open the file while checking if it exists
+    python_content = None
+    vul_content = None
+    # try to open the files while checking if they exists
     try:
         with open(pythonFile, 'r') as file:
             python_content = file.read()
-
-            print(python_content)
     except FileNotFoundError:
         print(f"The file '{pythonFile}' does not exist.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    ast_tree = ast.parse(python_content)
-    ast_json = json.dumps(ast_tree, default=lambda x: x.__dict__)
+    try:
+        with open(vulFile, 'r') as file:
+            vul_content = json.load(file)
+    except FileNotFoundError:
+        print(f"The file '{vulFile}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    print(ast_json[3])
+    
+    ast_tree = ast.parse(python_content)
+    ast_json = json.loads(json.dumps(ast_tree, default=lambda x: x.__dict__))
+
+    vulnerabilities = None
+    if (len(vul_content) == 0):
+        vulnerabilities = []
+
+
+    with open(outputFile, "w", encoding="utf-8") as outfile:
+        json.dump(vulnerabilities, outfile, indent=1)
+
     sys.exit(1)
 
 if __name__ == "__main__":
