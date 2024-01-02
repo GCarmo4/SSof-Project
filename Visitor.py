d8acd0b9a14967d16a55543a296526d6e68e6e17
Gonçalo Carmo <goncalo.n.carmo@tecnico.ulisboa.pt>
2024-01-02 23:15:46 +0000

    fixed visitor

--- Visitor.py
+++ Visitor.py
@@ -22,7 +22,7 @@ class Visitor (BaseVisitor):
             self.vulnerabilities.add_vulnerability_by_multilabel(iflow, Sink(name,lineno))
 
     def visit_Constant(self, node):
-        multilabel = Multilabel([])
+        multilabel = Multilabel([policy.patterns])
         return multilabel
 
     def visit_Name(self, node):
@@ -34,7 +34,7 @@ class Visitor (BaseVisitor):
         patterns_for_sink = []
         patterns_for_sink = self.policy.get_pattern_sink(node.id)
         if len(patterns_for_sink) != 0:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
             multilabel.add_patterns_sink(patterns_for_sink, Sink(node.id, node.lineno), self.vulnerabilities)
 
 
@@ -45,27 +45,27 @@ class Visitor (BaseVisitor):
     def visit_BinOp(self, node):
         multilabel = self.visit(node.left)
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
         right_multilabel = self.visit(node.right)
         if right_multilabel is None:
-            right_multilabel = Multilabel([])
+            right_multilabel = Multilabel([self.policy.patterns])
         multilabel = multilabel.combine(right_multilabel, self.vulnerabilities)
         return multilabel
 
     def visit_UnaryOp(self, node):
         multilabel = self.visit(node.operand)
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
         return multilabel
 
     def visit_BoolOp(self, node):
         multilabel = self.visit(node.values[0])
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
         for i in range(1, len(node.values)):
             val_multilabel = self.visit(node.values[i])
             if val_multilabel is None:
-                val_multilabel = Multilabel([])
+                val_multilabel = Multilabel([self.policy.patterns])
             multilabel = multilabel.combine(val_multilabel, self.vulnerabilities)
         return multilabel
 
@@ -74,19 +74,19 @@ class Visitor (BaseVisitor):
         for comp in node.comparators:
             comp_multilabel = self.visit(comp)
             if comp_multilabel is None:
-                comp_multilabel = Multilabel([])
+                comp_multilabel = Multilabel([self.policy.patterns])
             multilabel = multilabel.combine(comp_multilabel, self.vulnerabilities)
         return multilabel
 
     def visit_Call(self, node):
         multilabel = self.visit(node.func)
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
 
         for arg in node.args:
             arg_multilabel = self.visit(arg)
             if arg_multilabel is None:
-                arg_multilabel = Multilabel([])
+                arg_multilabel = Multilabel([self.policy.patterns])
             multilabel = multilabel.combine(arg_multilabel, self.vulnerabilities)
         
         return multilabel
@@ -94,7 +94,7 @@ class Visitor (BaseVisitor):
     def visit_Attribute(self, node):
         value_multilabel = self.visit(node.value)
         if value_multilabel is None:
-            value_multilabel = Multilabel([])
+            value_multilabel = Multilabel([self.policy.patterns])
 
         return value_multilabel
 
@@ -102,13 +102,13 @@ class Visitor (BaseVisitor):
     def visit_Expr(self, node):
         multilabel = self.visit(node.value)
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
         return multilabel
 
     def visit_Assign(self, node):
         multilabel = self.visit(node.value)
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
         
         for target in node.targets:
             self.multilabelling.update_multilabel_for_name(target, multilabel)
@@ -118,7 +118,7 @@ class Visitor (BaseVisitor):
     def visit_If(self, node):
         multilabel = self.visit(node.test)
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
 
         return multilabel
 
@@ -127,7 +127,7 @@ class Visitor (BaseVisitor):
 
         multilabel = self.visit(node.test)
         if multilabel is None:
-            multilabel = Multilabel([])
+            multilabel = Multilabel([self.policy.patterns])
         
 
         return multilabel
